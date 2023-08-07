23d20bdf63b64d6675280da3aba753db530fb023
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-08-07 17:50:31 -0400

    changed file structure, started about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/law.jpg'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +9,9 @@ function App() {
   return (
     <>
       <div>
+        <h1>Test</h1>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="./assets/law.jpg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import profile picture here
+
+export default function About() {
+    return( 
+        <div>
+        <section className="proheader">
+            <h1>About</h1>
+            <img className="propic" src="" alt="A photo of me!"></img>
+        </section>
+        <section className="desc">
+            <p>Hello, my name is Aubrey McKinney, </p>
+        </section>
+        </div>
+    )
+}
