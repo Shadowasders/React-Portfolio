47e373000b59be2e4d0193fddeba2cf76db3cf41
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-08-14 18:16:57 -0400

    fixed resume upload button, need to fix navbar burger and it's done!

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 import 'bulma/css/bulma.min.css'
-//figure out why link colors aren't changing, they should not be blue
 //impliment navbar burger, shouldnt be too hard same as LoTP See that and copy no biggie
 
 export default function Nav() {
@@ -9,7 +8,7 @@ export default function Nav() {
       <div className='hero-body'>
       <nav className='navbar'>
         <div id='' className='navbar-menu'>
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar">
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="dtarg">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -17,7 +16,7 @@ export default function Nav() {
           <div id='navbar' className='navbar-start'>
             <p className='navbar-item name title has-text-white'>Aubrey McKinney</p>
           </div>
-        <div className='navbar-end'>
+        <div className='navbar-end dtarg'>
             <a className='navbar-item'><Link to="/">About</Link></a>
             <a className='navbar-item'><Link to="/projects">Projects</Link></a>
             <a className='navbar-item'><Link to="/contact">Contact</Link></a>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,16 +1,11 @@
-//add download for resume?
-//add sections for skills 
-//???
-//profit
-//get help w/this
-// import resume from '../assets/Development/resume.pdf'
+import resume from '../assets/resume.pdf'
 
 export default function Resume() {
     return (
         <section className="columns is-centered">
         <div className="contact column is-half">
             <h1 className="title mt-2 has-text-centered has-text-white">Resume</h1>
-            <a href="resume"><button className="button is-fullwidth mb-4">Download here!</button></a>
+            <a href={resume} target="_blank"><button className="button is-fullwidth mb-4">Download here!</button></a>
             <div className="content skills has-text-white">
                 <h1 className="title has-text-centered has-text-white mt-2">Proficiencies</h1>
             <ul>
