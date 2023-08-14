4d88fcf79e361d409e500611cfe2d8fe5a71249a
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-08-14 02:33:13 -0400

    footer implmented, styling to come

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import Nav from './components/navbar'
+import Nav from './components/navbar';
 //how to get footer in here??
+import Footer from './components/footer';
   export default function App() {
     return (
       <>
       <Nav />
       <Outlet />
+      <Footer />
       </>
     );
   }
