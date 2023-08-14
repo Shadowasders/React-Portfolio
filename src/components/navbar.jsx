9b8f2e7e3e8491e971fa87efcc109e883f0b031a
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-08-14 18:33:31 -0400

    everything done besides navbar burger, to fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,30 +1,31 @@
 import { Link } from 'react-router-dom'
 import 'bulma/css/bulma.min.css'
-//impliment navbar burger, shouldnt be too hard same as LoTP See that and copy no biggie
+import './burger'
+//impliment navbar burger, shouldnt be too hard same as LoTP See that and copy no biggie --it was harder
 
 export default function Nav() {
   return (
     <section className='hero is-medium-with-navbar headerbg'>
       <div className='hero-body'>
-      <nav className='navbar'>
-        <div id='' className='navbar-menu'>
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="dtarg">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      </a>
-          <div id='navbar' className='navbar-start'>
-            <p className='navbar-item name title has-text-white'>Aubrey McKinney</p>
+        <nav className='navbar'>
+          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="dtarg">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+          <div className='navbar-menu dtarg'>
+            <div id='navbar' className='navbar-start'>
+              <p className='navbar-item name title has-text-white'>Aubrey McKinney</p>
+            </div>
+            <div className='navbar-end'>
+              <a className='navbar-item'><Link to="/">About</Link></a>
+              <a className='navbar-item'><Link to="/projects">Projects</Link></a>
+              <a className='navbar-item'><Link to="/contact">Contact</Link></a>
+              <a className='navbar-item'><Link to="/resume">Resume</Link></a>
+            </div>
           </div>
-        <div className='navbar-end dtarg'>
-            <a className='navbar-item'><Link to="/">About</Link></a>
-            <a className='navbar-item'><Link to="/projects">Projects</Link></a>
-            <a className='navbar-item'><Link to="/contact">Contact</Link></a>
-            <a className='navbar-item'><Link to="/resume">Resume</Link></a>
-        </div>
+        </nav>
       </div>
-    </nav>
-    </div>
     </section>
-  ) 
+  )
 } 
